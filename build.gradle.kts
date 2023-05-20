a24851b4aa97416197a77d6ddd5de259d9bf28fa
Dylan Scott <dscott@hex.tech>
2023-05-19 19:55:21 -0700

    build

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+repositories {
+  mavenCentral()
+}
+
+plugins {
+  java
+  application
+  idea
+}
+
+dependencies {
+  implementation("org.duckdb:duckdb_jdbc:0.8.0")
+
+  implementation("org.apache.arrow:arrow-vector:12.0.0")
+  implementation("org.apache.arrow:arrow-c-data:12.0.0")
+  implementation("org.apache.arrow:arrow-jdbc:12.0.0")
+  implementation("org.apache.arrow:flight-grpc:12.0.0")
+}
